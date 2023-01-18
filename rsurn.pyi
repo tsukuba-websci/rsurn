e6448a13966bc79478c586d3cce6db9342128a4f
SUDA Mikihiro <s@sudame.net>
2023-01-18 18:53:27 +0900

    update: type stub

--- rsurn.pyi
+++ rsurn.pyi
@@ -1,1 +1,18 @@
-def sum_as_string(x: int, y: int) -> str: ...
+from typing import List, Tuple
+
+class Gene:
+    def __new__(cls: type["Gene"], rho: int, nu: int) -> "Gene": ...
+
+class Environment:
+    history: List[Tuple[int, int]]
+
+    def __new__(cls: type["Environment"], gene: Gene) -> "Environment": ...
+
+    def get_caller(self) -> int: ...
+
+    def get_callee(self, caller: int) -> int: ...
+
+    def interact(self, caller: int, callee: int) -> None: ...
+
+
+
