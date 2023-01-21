862a4ddbe70889e294edf5a30da09f2c8e1011c8
SUDA Mikihiro <s@sudame.net>
2023-01-21 18:14:26 +0900

    update: 最近度と親密度の遺伝子を加える

--- src/lib.rs
+++ src/lib.rs
@@ -43,13 +43,20 @@ impl Urns {
 pub struct Gene {
     pub rho: usize,
     pub nu: usize,
+    pub recentness: f64,
+    pub friendship: f64,
 }
 
 #[pymethods]
 impl Gene {
     #[new]
-    fn new(rho: usize, nu: usize) -> Self {
-        Self { rho, nu }
+    fn new(rho: usize, nu: usize, recentness: f64, friendship: f64) -> Self {
+        Self {
+            rho,
+            nu,
+            recentness,
+            friendship,
+        }
     }
 }
 
@@ -61,6 +68,9 @@ pub struct Environment {
 
     /** callerとして選択される可能性のあるエージェント群の (agent_id, weight) の組 */
     weights: FxHashMap<usize, usize>,
+
+    /** 最近度 */
+    recentnesses: Vec<FxHashMap<usize, usize>>,
 
     /** (caller, callee) で表される生成データ */
     #[pyo3(get)]
@@ -105,11 +115,17 @@ impl Environment {
             candidates.insert(agent_id, gene.nu + 2);
         }
 
+        let mut recentnesses = vec![];
+        for _ in 0..(2 + 2 * (gene.nu + 1)) {
+            recentnesses.push(FxHashMap::default());
+        }
+
         Environment {
             history: vec![],
             gene,
             urns,
             weights: candidates,
+            recentnesses,
         }
     }
 
@@ -140,6 +156,8 @@ impl Environment {
 
     pub fn interact(&mut self, caller: usize, callee: usize) -> Option<()> {
         self.history.push((caller, callee));
+        *self.recentnesses[caller].entry(callee).or_insert(0) += 1;
+        *self.recentnesses[callee].entry(caller).or_insert(0) += 1;
 
         if !self.weights.contains_key(&callee) {
             self.add_novelty(callee);
@@ -165,24 +183,50 @@ impl Environment {
         let mut ret = vec![];
 
         let urn = self.urns.get(me).unwrap();
+        let recentness = self.recentnesses.get(me).unwrap();
 
-        // 計算用に壺のコピーを作成
+        // 計算用にコピーを作成
         let mut urn = urn.clone();
+        let mut recentness = recentness.clone();
 
-        // 相手は選択しないよう予め重みを0にする
-        *urn.entry(opponent).or_insert(0) = 0;
+        let mut weights_map = FxHashMap::default();
 
-        let candidates: Vec<usize> = urn.keys().map(|v| v.to_owned()).collect();
-        let weights: Vec<usize> = urn.values().map(|v| v.to_owned()).collect();
+        let max_friendship = *urn.values().max().unwrap() as f64;
+        for (agent, weight) in urn {
+            *weights_map.entry(agent).or_insert(0.0) +=
+                (weight as f64 / max_friendship) * self.gene.friendship;
+        }
 
-        let mut dist = WeightedIndex::new(weights.to_vec())?;
+        let mut max_recentness = *recentness.values().max().unwrap() as f64;
+        if max_recentness == 0.0 {
+            max_recentness = 1.0;
+        }
+        for (agent, weight) in recentness {
+            *weights_map.entry(agent).or_insert(0.0) +=
+                (weight as f64 / max_recentness) * self.gene.recentness;
+        }
+
+        let candidates: Vec<usize> = weights_map.keys().copied().collect();
+        let mut weights = Vec::from_iter(weights_map.values().cloned());
+
+        let min_weight = weights.iter().fold(f64::NAN, |m, v| v.min(m));
+        if min_weight < 0.0 {
+            weights = weights.iter().map(|w| *w - min_weight).collect();
+        }
+
+        let mut dist = WeightedIndex::new(weights)?;
+        // 相手に相手自身を紹介しないよう重みを0にする
+        let _ = dist.update_weights(&[(
+            candidates.iter().position(|c| *c == opponent).unwrap(),
+            &0.0,
+        )]);
 
         for _ in 0..(self.gene.nu + 1) {
             let i = dist.sample(&mut rng);
             ret.push(candidates[i]);
 
             // 一度選択したものは重みを0にして重複して選択されないようにする
-            let _ = dist.update_weights(&[(i, &0)]);
+            let _ = dist.update_weights(&[(i, &0.0)]);
         }
 
         Ok(ret)
@@ -192,6 +236,7 @@ impl Environment {
         for _ in 0..(self.gene.nu + 1) {
             let i = self.urns.add_urn();
             self.urns.add(agent_id, i);
+            self.recentnesses.push(FxHashMap::default());
         }
         *self.weights.entry(agent_id).or_insert(0) += self.gene.nu + 1;
     }
@@ -212,7 +257,12 @@ mod test {
 
     #[test]
     fn sample_program() {
-        let gene = Gene { rho: 3, nu: 4 };
+        let gene = Gene {
+            rho: 3,
+            nu: 4,
+            recentness: 1.0,
+            friendship: 0.0,
+        };
         let mut env = Environment::new(gene);
 
         for _ in 0..1000 {
