6398a6a0fd820b7d8078e359c6c5e1dae5953fe4
ciaran-regan-ie <ciaranregan123@gmail.com>
2023-08-28 08:01:50 +0900

    update initial history

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,7 @@ impl Environment {
         }
 
         Environment {
-            history: vec![],
+            history: vec![(0,1)],
             gene,
             urns,
             weights: candidates,
